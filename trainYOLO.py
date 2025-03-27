366871451534eb620dc7304ff6d249ab39d3fbf5
Ckontogi24 <ckontogiannis24@gmail.com>
2025-03-27 12:36:52 +0200

    Configured File

--- trainYOLO.py
+++ trainYOLO.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO("yolo11n-seg.pt")  # YOLO11 medium μοντέλο για segmentation
-model.train(data="/home/roboticslab/Documents/crack_segmentation_merged_dataset/data.yaml", epochs=300, imgsz=448, batch=2)
+model.train(data="/second_ext4/roboticslab/crack_segmentation_merged_dataset/data.yaml", epochs=300, imgsz=448, batch=2)
